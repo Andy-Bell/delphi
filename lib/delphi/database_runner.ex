a3c73a00752537a502d875a578d914ba1b318a31
danielschambers <daniel.s.chambers@gmail.com>
2016-07-28 13:52:58 +0100

    database query can now accept table argument

--- lib/delphi/database_runner.ex
+++ lib/delphi/database_runner.ex
@@ -7,15 +7,15 @@ defmodule Delphi.DatabaseRunner do
     Delphi.Supervisor.start_link
   end
 
-  def database_insert(conn, params) do
-    q = Query.table("test")
+  def database_insert(conn, table, params) do
+    q = Query.table(table)
     |> Query.insert(params)
     |> Database.run(conn)
 
   end
 
-  def database_view(conn) do
-    q = Query.table("test")
+  def database_view(conn, table) do
+    q = Query.table(table)
     |> Database.run(conn)
 
   end
