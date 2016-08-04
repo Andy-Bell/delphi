ea42fcb3965a322b0ecf668ff0aaac7014522bdc
mtaner <mervetaner88@gmail.com>
2016-08-04 15:21:40 +0100

    happy now merve seed file changed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,6 +1,6 @@
 
-Delphi.Repo.insert! %Delphi.UrlWriter{url: "https://thoughtbot.com"}
-Delphi.Repo.insert! %Delphi.UrlWriter{url: "http://www.marvel.com"}
-Delphi.Repo.insert! %Delphi.UrlWriter{url: "https://www.independent.co.uk"}
-Delphi.Repo.insert! %Delphi.UrlWriter{url: "https://www.makersacademy.com"}
-Delphi.Repo.insert! %Delphi.UrlWriter{url: "http://www.artparasites.com"}
+Delphi.Repo.insert! %Delphi.UrlTable{url: "https://thoughtbot.com"}
+Delphi.Repo.insert! %Delphi.UrlTable{url: "http://www.marvel.com"}
+Delphi.Repo.insert! %Delphi.UrlTable{url: "https://www.independent.co.uk"}
+Delphi.Repo.insert! %Delphi.UrlTable{url: "https://www.makersacademy.com"}
+Delphi.Repo.insert! %Delphi.UrlTable{url: "http://www.artparasites.com"}
