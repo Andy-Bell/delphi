79c6b307baf79113b884461cb5d36a97edc4f0fe
danielschambers <daniel.s.chambers@gmail.com>
2016-08-04 09:31:53 +0100

    Partial tests

--- spec/delphi/url_writer_spec.exs
+++ spec/delphi/url_writer_spec.exs
@@ -1,17 +1,20 @@
 defmodule UrlWriterSpec do
   use ESpec
 
+  before do
+    allow(UrlScraper).to accept(:search_urls, fn(a) -> list end)
+    allow(UrlWriter).to accept(:write, fn(a) -> :ok end)
+    allow(ScraperController).to accept(:scrape_page, fn(a) -> :ok end)
+  end
+
   let :list do
     ["http://www.test.com", "http://www.test.com/test", "http://www.test.com/all/test", "http://www.test.com/lulz", "http://www.test.com/hi/merve/yay/"]
   end
 
   it "adds a url to the table" do
-    allow(UrlScraper).to accept(:search_urls, fn(a) -> list end)
-    allow(UrlWriter).to accept(:write, fn(a) -> :ok end)
-    allow(ScraperController).to accept(:scrape_page, fn(a) -> :ok end)
-
     expect UrlWriter.add_url_to_table("http://www.test.com")
     |> to(eq :ok )
+    expect UrlWriter |> to(accepted :)
   end
 
 end
